00d8286d01fb923b4b59e28e5ddb423fc8d19b03
Carla Holcomb <carla.programmer@gmail.com>
2020-01-02 02:04:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,39 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+
+ 
+  attr_accessor :name, :grade
+  attr_reader :id
+ 
+  def initialize(name, album, id=nil)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+ 
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
+ 
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, grade) 
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.grade)
+ 
+  end
+ 
+
+  
   
 end
